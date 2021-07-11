1a37287588375618abf6846718a636a475326ae1
yusantus <yusantus.2021@gmail.com>
2021-07-11 13:31:38 +0200

    version 0.0.10

--- pyyhtml/content.py
+++ pyyhtml/content.py
@@ -2,11 +2,12 @@ from .interfaces import FromFile
 
 
 class FromTXT(FromFile):
-    def __init__(self, filepath):
+    def __init__(self, filepath, new_lines = False):
         super().__init__(filepath)
         with open(self.filepath, "r") as readfile:
             self.lines = readfile.readlines()
-            for i in range(len(self.lines)):
-                self.lines[i] = self.lines[i].replace("\n", "<br>")
+            if new_lines:
+                for i in range(len(self.lines)):
+                    self.lines[i] = self.lines[i].replace("\n", "<br>")
     def get_content_list(self):
         return self.lines
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as readme:
 
 setup(
     name = "pyyhtml",
-    version = "0.0.8",    
+    version = "0.0.10",    
     description = "A simple HTML pseudo for Python",
     py_modules = ["pyyhtml"],
     packages = find_packages(),
