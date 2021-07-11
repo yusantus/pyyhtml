044ea37501f1aafb78d119e38b40120d6ef2b8bf
yusantus <yusantus.2021@gmail.com>
2021-07-11 13:44:06 +0200

    version 0.0.13

--- build/lib/pyyhtml/fileio.py
+++
@@ -1,5 +0,0 @@
-def write_list_to_file(filepath: str, content: list):
-    with open(filepath, "w", encoding="UTF-8") as writefile:
-        writefile.write("<!doctype html>\n")
-        for line in content:
-            writefile.write(line + "\n")
